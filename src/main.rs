241c14a236152cc777c744b140dc4534d96861d8
David James <davidcjames@gmail.com>
2021-08-27 17:22:49 -0400

    Try async version

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,28 @@ use dialoguer::Confirm;
 
 use std::io;
 
-fn main() {
-    get_confirmation().unwrap();
+#[tokio::main]
+async fn main() {
+    get_conf_async().await.unwrap();
 }
 
-fn get_confirmation() -> io::Result<bool> {
+pub async fn get_conf_async() -> io::Result<bool> {
+    if Confirm::new()
+        .with_prompt("Do you want to continue?")
+        .interact()?
+    {
+        println!("Continuing...");
+    } else {
+        println!("Cancelling...");
+    }
+    Ok(true)
+}
+
+pub fn main_sync() {
+    get_conf().unwrap();
+}
+
+pub fn get_conf() -> io::Result<bool> {
     if Confirm::new()
         .with_prompt("Do you want to continue?")
         .interact()?
